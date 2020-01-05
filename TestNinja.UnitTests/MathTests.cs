980dbcd2f9a6ea9687049b113a492d77356e9418
SieciechK <sieciech.k@gmail.com>
2020-01-05 21:50:08 +0100

    refactoring to use NUnit SetUp

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -8,12 +8,18 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
-            var math = new Math();
-
-            var result = math.Add(1, 2);
+            var result = _math.Add(1, 2);
 
             Assert.That(result, Is.EqualTo(3));
         }
@@ -21,9 +27,7 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnFirstArgument()
         {
-            var math = new Math();
-
-            var result = math.Max(2, 1);
+            var result = _math.Max(2, 1);
 
             Assert.That(result, Is.EqualTo(2));
         }
@@ -31,9 +35,7 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
         {
-            var math = new Math();
-
-            var result = math.Max(1, 2);
+            var result = _math.Max(1, 2);
 
             Assert.That(result, Is.EqualTo(2));
         }
@@ -41,9 +43,7 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_ArgumentsAreEqual_ReturnFirstArgument()
         {
-            var math = new Math();
-
-            var result = math.Max(1, 1);
+            var result = _math.Max(1, 1);
 
             Assert.That(result, Is.EqualTo(1));
         }
