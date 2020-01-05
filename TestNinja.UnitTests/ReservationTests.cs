679845b121e0d88a0b02ef4cc52f35eb6558fb5c
SieciechK <sieciech.k@gmail.com>
2020-01-05 20:11:05 +0100

    initial commit - first exercise done with NUnit and MSpec

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture()]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCanceledBy_Admin_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+            //Act
+            var result = reservation.CanBeCancelledBy(new User {IsAdmin = true});
+            //Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCanceledBy_Creator_ReturnsTrue()
+        {
+            var creator = new User();
+            var reservation = new Reservation {MadeBy = creator};
+
+            var result = reservation.CanBeCancelledBy(creator);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCanceledBy_DifferentUser_ReturnsFalse()
+        {
+            var reservation = new Reservation {MadeBy = new User()};
+
+            var result = reservation.CanBeCancelledBy(new User());
+
+            Assert.That(result, Is.False);
+        }
+    }
+}
---
+++ TestNinja.UnitTests/ReservationsSpecs.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Machine.Specifications;
+using TestNinja.Fundamentals;
+// ReSharper disable InconsistentNaming
+// ReSharper disable ArrangeTypeMemberModifiers
+
+namespace TestNinja.UnitTests
+{
+    public class ReservationsSpecs
+    {
+        [Subject(typeof(Reservation))]
+        public class When_cancelling_reservation_by_admin
+        {
+            static Reservation subject;
+            static bool can_cancel;
+
+            Establish context = () =>
+                subject = new Reservation();
+
+            Because of = () =>
+                can_cancel = subject.CanBeCancelledBy(new User {IsAdmin = true});
+
+            It should_be_possible = () =>
+                can_cancel.ShouldBeTrue();
+
+        }
+
+        [Subject(typeof(Reservation))]
+        public class When_canceling_reservation_by_creator
+        {
+            static Reservation subject;
+            static bool can_cancel;
+            static User user = new User();
+
+            Establish context = () =>
+                subject = new Reservation {MadeBy = user};
+
+            Because of = () =>
+                can_cancel = subject.CanBeCancelledBy(user);
+
+            It should_be_possible = () =>
+                can_cancel.ShouldBeTrue();
+        }
+
+        [Subject(typeof(Reservation))]
+        public class when_cancelling_reservation_by_different_user
+        {
+            static Reservation subject;
+            static bool can_cancel;
+
+            Establish context = () =>
+                subject = new Reservation {MadeBy = new User()};
+
+            Because of = () => can_cancel = subject.CanBeCancelledBy(new User());
+
+            It should_not_be_possible = () => 
+                can_cancel.ShouldBeFalse();
+        }
+
+}
+
+}
